7aaee91c101591f02748f87ce30da61f5e01b57c
Cherenov Mark <109592374+EnoughsEnough@users.noreply.github.com>
2023-03-10 17:01:01 +0300

    Update AboutPage.async.tsx

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -1,7 +1,6 @@
 import { lazy } from 'react';
 
 export const AboutPageAsync = lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! 
+    // @ts-ignore 
     setTimeout(() => resolve(import('./AboutPage')), 1500);
 }));
