dd72f8950dff51163bce92e3794ca136836a8028
Mark <mcherenov@mail.ru>
2023-03-12 15:38:38 +0300

    add ArticlesPage, ArticleDetailPage, PrivatesRoutes

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -1,6 +1,6 @@
 import { lazy } from 'react';
 
 export const AboutPageAsync = lazy(() => new Promise((resolve) => {
-    // @ts-ignore 
+    // @ts-ignore
     setTimeout(() => resolve(import('./AboutPage')), 1500);
 }));
